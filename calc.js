992dd964245b16ec52488e968453147e70603ae1
dmatiasr <matu_dmr@hotmail.com>
2016-11-02 22:32:21 -0300

    Functions to add Numbers and clr

--- calc.js
+++ calc.js
@@ -2,3 +2,15 @@ function clr() {
 	v = document.getElementById('values');
 	v.value ="";
 }
+function deleteOne() {
+	v= document.getElementById('values');
+	oldValue= v.value;
+	newValue= oldValue.slice(0,(oldValue.length-1))
+	v.value=newValue;
+}
+function addNumber(numb) {
+	v= document.getElementById('values');
+	oldValue=v.value;
+	newValue=oldValue+numb;
+	v.value=newValue;
+}
